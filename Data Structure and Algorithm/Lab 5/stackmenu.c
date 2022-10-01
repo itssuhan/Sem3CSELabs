aa52de848eda4f782c4c82c2ab19ee657616ddce
abcd <abcd@gmail.com>
2022-10-01 16:23:51 +0530

    Added Lab 5 Programs

--- Data Structure and Algorithm/Lab 5/stackmenu.c
+++ Data Structure and Algorithm/Lab 5/stackmenu.c
@@ -1,90 +1,80 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-#define STACK_SIZE 5
-typedef struct
-{
-    char arr[STACK_SIZE];
+typedef struct {
+    char st[10];
     int tos;
-} STACK;
+} Stack;
 
-int isEmpty(STACK s)
-{
-    return s.tos == -1;
-}
-
-int isFull(STACK s)
-{
-    return s.tos == STACK_SIZE;
-}
-
-void push(STACK *s,char n)
-void push(char n,STACK *s)
-{
-    if (s->tos==MAX-1)
-    {
-        printf("Stack Overflow \n");
-    }
-    else
-    {
-        s->tos++;
-        s->stack[s->tos]=x;
-    }
-}
-
-char pop(STACK *s)
-{
-    if (isEmpty(*s))
-    {
-        printf("Stack Underflow, Terminating\n");
+int isFull(Stack s) {
+    if ((s.tos+1) > 9)
         return 1;
+    return 0; 
+}
+
+int isEmpty(Stack s) {
+    if (s.tos == -1)
+        return 1;
+    return 0;
+}
+
+
+Stack push(Stack s) {
+    char x;
+    if (isFull(s))
+        printf("Error. Overflow ");
+    else {
+        printf("Enter character to push onto stack ");
+        scanf(" %c",&x);
+        s.tos = s.tos + 1;
+        s.st[s.tos] = x;
     }
-    return s->arr[s->tos--];
+    return s;
 }
 
-char peak(STACK s)
-{
-    return s.arr[s.tos];
+Stack pop(Stack s) {
+    if (isEmpty(s))
+        printf("Error. Underflow ");
+    else {
+        printf("The popped element is: %c",s.st[s.tos]);
+        s.st[s.tos] = -5;
+        s.tos-=1;
+    }
+    return s;
 }
 
-void display(STACK s)
-{
-    printf("[ ");
-    for (int i = 0; i <= s.tos; i++)
-        printf("%c ", s.arr[i]);
-    printf("\n");
+void display(Stack s) {
+    int i;
+    printf("[");
+    for (i=0;i<=s.tos;i++) {
+        printf("%c ",s.st[i]);
+    }
+    printf("] \n");
 }
 
-int main()
-{
+int main() {
 
+    Stack s;
+    s.tos = -1;
     int input;
-    STACK *s;
-    while (input != 3)
-    {
-        printf("Do you want to push, pop or exit? (1/2/3) ");
-        scanf("%d", &input);
-        char out;
-        switch (input)
-        {
-        case 1:
-            printf("Enter the character to push: ");
-            char in;
-            scanf("%c", &in);
-            push(s, in);
-            display(*s);
-            break;
-        case 2:
-            out = pop(s);
-            printf("Popped %c \n", out);
-            display(*s);
-            break;
+    do {
+        printf("\nEnter 1 to push, 2 to pop, 3 to display, and -1 to exit \n");
+        scanf("%d",&input);
 
-        case 3:
-            break;
-        default:
-            break;
+        if (input == 1) {
+            s = push(s);
         }
-    }
+
+        else if (input == 2) {
+            s = pop(s);
+        }
+
+        else if(input == 3) {
+            display(s);
+        }
+
+        else {
+            input = -1;
+        }
+
+    } while (input != -1);
+    return 0;
 }
